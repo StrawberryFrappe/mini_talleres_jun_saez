6a378280c949f2ee2a8934722e049a8e6597c9c4
Strawberry Frappé <65193599+StrawberryFrappe@users.noreply.github.com>
2024-10-08 22:56:03 -0300

    Create Calculator.vue

---
+++ src/components/Calculator.vue
@@ -0,0 +1,27 @@
+<script setup>
+
+import {ref} from "vue";
+
+const props = defineProps({
+  studentGrades: {
+    type: Object,
+    required: true,
+    default: () => ({
+      grade1:ref(0),
+      grade2:ref(0),
+      grade3:ref(0),
+      grade4:ref(0),
+      grade5:ref(0),
+      grade6:ref(0)
+    })
+  }
+})
+</script>
+
+<template>
+  
+</template>
+
+<style scoped>
+
+</style>
