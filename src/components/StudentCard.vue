e2c7541b98841ac0a6e045eb4ce5c5901ed5cbf9
Strawberry Frappé <65193599+StrawberryFrappe@users.noreply.github.com>
2024-10-09 02:23:20 -0300

    Create StudentCard.vue

---
+++ src/components/StudentCard.vue
@@ -0,0 +1,37 @@
+<script setup>
+
+const props = defineProps({
+  student:Object,
+  key: String
+})
+
+</script>
+
+<template>
+  <main>
+    <section class="student">
+      <p class="name">{{student.name}}</p>
+      <p class="level">{{student.level}}</p>
+      <p class="grades">{{student.grades}}</p>
+    </section>
+  </main>
+</template>
+
+<style scoped>
+.student {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+}
+
+.name {
+  text-align: left;
+}
+
+.level {
+  text-align: center;
+}
+
+.grades {
+  text-align: right;
+}
+</style>
