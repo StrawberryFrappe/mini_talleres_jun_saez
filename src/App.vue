1c37b360caa74003d1d3571c35b56404d3ed06ff
Strawberry Frappé <65193599+StrawberryFrappe@users.noreply.github.com>
2024-10-20 23:37:03 -0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,28 @@
 <script setup>
 
-import {createRouter as $router} from "vue-router/dist/vue-router.esm-browser.js";
+import router from './main.js'
+import {ref} from "vue";
+
+const guess = ref('')
 </script>
 
 <template>
   <div>
-    <button @click="$router.push('/')">uno</button>
-    <button @click="$router.push('/dos')">dos</button>
-    <button @click="$router.push('/tres')">tres</button>
-    <button @click="$router.push('/cuatro')">cuatro</button>
-    <button @click="$router.push('/cinco')">cinco</button>
+    <button @click="router.push('/')">boton</button>
+    <button @click="router.push('/dos')">boton</button>
+    <button @click="router.push('/tres')">boton</button>
+    <button @click="router.push('/cuatro')">boton</button>
+    <button @click="router.push('/cinco')">boton</button>
   </div>
   <router-view></router-view>
+  <input type="text" v-model="guess">
+  <button @click=" function (){
+    if (guess === 'Tengo ganas de un cafe'){
+      guess = 'correcto'
+    } else {
+      guess.value = 'Intenta nuevamente'
+    }
+  }">comprobar</button>
 </template>
 
 <style scoped>
